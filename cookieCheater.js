ba1ffdecccd3b9f039e40b9c74557cf3c8afbd13
Tanpi <usmcinu@gmail.com>
2016-02-12 22:10:38 +0100

    bot now clicks golden cookies

--- cookieCheater.js
+++ cookieCheater.js
@@ -19,7 +19,7 @@ CBOT.buyStuff = function(){
 
     for (var i = 0; i < CBOT.objects.length; i++){
         if (CM.Cache.Objects[CBOT.objects[i][1]].color == "Green"){
-            $(CBOT.objects[i][0]).click()
+            $(CBOT.objects[i][0]).click();
         }
     }
 }
@@ -29,6 +29,10 @@ CBOT.autoClick = function(){
     CBOT.autoObject = setInterval(function(){
         Game.ClickCookie();
     }, 1);
+}
+
+CBOT.autoGoldenCookie = function(){
+    $('#goldenCookie').click();
 }
 
 CBOT.stopClick = function(){
@@ -46,5 +50,16 @@ CBOT.stopAutoBuy = function(){
     clearInterval(CBOT.autoBuyObject);
 }
 
-CBOT.insertHTML = "<button id='CBOTstartClick' onclick='CBOT.autoClick()'>Start click</button> <button id='CBOTstopClick' onclick='CBOT.stopClick()'>Stop click</button><br><button id='CBOTautoBuy' onclick='CBOT.startAutoBuy()'>AutoBuy</button> <button id='CBOTstopBuy' onclick = 'CBOT.stopAutoBuy'>Stop AutoBuy</button>";
+CBOT.startGoldenClicker = function(){
+    clearInterval(CBOT.autoGoldenCookieObject);
+    CBOT.autoGoldenCookieObject = setInterval(function(){
+        CBOT.autoGoldenCookie();
+    }, 500)
+}
+
+CBOT.stopGoldenClicker = function(){
+    clearInterval(CBOT.autoGoldenCookieObject);
+}
+
+CBOT.insertHTML = "<button id='CBOTstartClick' onclick='CBOT.autoClick()'>Start click</button> <button id='CBOTstopClick' onclick='CBOT.stopClick()'>Stop click</button><br><button id='CBOTautoBuy' onclick='CBOT.startAutoBuy()'>AutoBuy</button> <button id='CBOTstopBuy' onclick = 'CBOT.stopAutoBuy'>Stop AutoBuy</button><br><button id='CBOTstartAutoGoldCookie' onclick='CBOT.startGoldenClicker()'>auto GCookie</button> <button id='CBOTstopAutoGoldCookie' onclick = 'stopGoldenClicker'>stop auto GCookie</button>";
 document.getElementById("comments").insertAdjacentHTML("beforeBegin", "<div id='CBOTControl' style='z-index: 99999999; background-color: black; position: relative; top: 0px; left: 100px; width: 175px; height: 50px;'>" + CBOT.insertHTML + " </div>");
